040f0d1dfde3b318678afdf3131cd48d4ccf3fa9
bryskiewiczr <bryskiewiczr@protonmail.com>
2022-05-10 01:47:49 +0200

    feat: change head

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,32 @@
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      title: 'Tell me more!',
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Tell me more details about the role.',
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content:
+            'tell me more about the job, more details about the job, linkedin recruiter, linkedin headhunter, tell me more',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/icon.png' }],
+    }
+  },
 }
 </script>
 
